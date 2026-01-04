f3c3e4a15832eb21f1b9c634fef15dbd2bc1c84e
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-04 16:22:55 -0500

    Update main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{
+    post, get, handler,
+    listener::TcpListener,
+    web::{Json, Path},
+    Route, Server
+};
+
+pub mod inputs;
+pub mod outputs;
+
+use inputs::CreateWebsiteInput;
+use outputs::CreateWebsiteOutput;
+
+#[handler] // get request handler
+fn get_website(Path(name): Path<String>) -> String{
+    format!("BetterUptime: {name}")
+}
+
+#[handler] // post request handler
+async fn create_website(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
+    let response = CreateWebsiteOutput {
+        url: data.url
+    };
+    Json(response)
+}
+
+#[tokio::main]  // Tokio main is a macro that is used for the main
+//function to run on the main thread of tokio runtime
+async fn main() -> Result<(), std::io::Error>{
+    let app = Route::new()
+    .at("/website/:name", get(get_website))
+    .at("/website", post(create_website));
+
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+    .name("BetterUptime")
+    .run(app)
+    .await
 }
