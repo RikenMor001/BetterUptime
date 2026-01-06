517f04dc3bacb60d18423ca07796d72460215bfe
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-06 17:33:25 -0500

    Add files via upload

---
+++ store/src/schema.rs
@@ -0,0 +1,40 @@
+// @generated automatically by Diesel CLI.
+
+pub mod sql_types {
+    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "website_status"))]
+    pub struct WebsiteStatus;
+}
+
+diesel::table! {
+    region (id) {
+        id -> Text,
+        name -> Text,
+    }
+}
+
+diesel::table! {
+    website (id) {
+        id -> Text,
+        url -> Text,
+        time_added -> Timestamptz,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::WebsiteStatus;
+
+    website_tick (id) {
+        id -> Text,
+        website_id -> Text,
+        region_id -> Text,
+        response_time_ms -> Int4,
+        status -> WebsiteStatus,
+    }
+}
+
+diesel::joinable!(website_tick -> region (region_id));
+diesel::joinable!(website_tick -> website (website_id));
+
+diesel::allow_tables_to_appear_in_same_query!(region, website, website_tick,);
