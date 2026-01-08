0fdfbdc70cc0aee3eec0443a88388d84e0b35af9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-07 22:18:03 -0500

    Update lib.rs

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,27 +1,18 @@
-pub mod schema;
-
+use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use diesel::result::ConnectionError;
-use diesel::PgConnection;
-use dotenvy::dotenv;
 use std::env;
+use std::error::Error;
+
+pub mod schema;
 
 pub struct Store {
     pub conn: PgConnection,
 }
 
 impl Store {
-    pub fn new() -> Result<Self, ConnectionError> {
-        dotenv().ok();
-
-        let db_url = env::var("DATABASE_URL")
-            .expect("DATABASE_URL must be set");
-
-        Self::from_url(&db_url)
-    }
-
-    pub fn from_url(database_url: &str) -> Result<Self, ConnectionError> {
-        let conn = PgConnection::establish(database_url)?;
+    pub fn new() -> Result<Self, Box<dyn Error>> {
+        let db_url = env::var("DATABASE_URL")?;
+        let conn = PgConnection::establish(&db_url)?;
         Ok(Self { conn })
     }
 }
