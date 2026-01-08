fef9b337199288eb88e3561dbc96dbdb356bb27b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-08 18:11:56 -0500

    Update lib.rs

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -2,19 +2,19 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::Connection;
 use std::env;
+use config::Config;
 
 pub mod schema;
-
+pub mod config;
 pub struct Store {
     pub conn: PgConnection
 }
 
 impl Store {
     fn default() -> Result<Self, ConnectionError> {
-        let db_url = env::var("DATABASE_URL")?;
-        .unwrap_or_else(|_| panic!("Please provide the database url enviornment variable"));
+        let config = Config::default()?;
 
-        let conn = PgConnection::establish(&db_url).unwrap_or_else(|_| panic!("Error connecting to the database"));
+        let conn = PgConnection::establish(&config.db_url).unwrap_or_else(|_| panic!("Error connecting to the database"));
         Ok(Self {
             conn
         })
