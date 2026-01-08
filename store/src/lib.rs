7f87467670d9864786f6fcbf99aa2f7810238719
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-08 18:51:57 -0500

    Update lib.rs

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,22 +1,4 @@
-use diesel::prelude::*;
-use diesel::pg::PgConnection;
-use diesel::Connection;
-use std::env;
-use config::Config;
-
 pub mod schema;
 pub mod config;
-pub struct Store {
-    pub conn: PgConnection
-}
-
-impl Store {
-    fn default() -> Result<Self, ConnectionError> {
-        let config = Config::default()?;
-
-        let conn = PgConnection::establish(&config.db_url).unwrap_or_else(|_| panic!("Error connecting to the database"));
-        Ok(Self {
-            conn
-        })
-    } 
-}
+pub mod store;
+pub mod models;
