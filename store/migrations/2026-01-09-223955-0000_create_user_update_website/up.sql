0f78629fd9a2bb1aef1281879698a78b817ca321
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 18:49:40 -0500

    Add files via upload

---
+++ store/migrations/2026-01-09-223955-0000_create_user_update_website/up.sql
@@ -0,0 +1,20 @@
+-- Your SQL goes here
+ALTER TABLE "website" ADD COLUMN "user_id" TEXT NOT NULL;
+
+--Alter table
+ALTER TABLE "website_tick" ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL
+DEFAULT CURRENT_TIMESTAMP;
+
+-- Create user table
+CREATE TABLE "User" {
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+
+    CONSTRAINT "user_pkey" PRIMARY KEY("id")
+};
+
+-- Add foreign key relationship between the user that is created now
+-- and the website that already exists
+ALTER TABLE "website" ADD CONSTRAINT "website_user_id_fkey" FOREIGN KEY
+("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
