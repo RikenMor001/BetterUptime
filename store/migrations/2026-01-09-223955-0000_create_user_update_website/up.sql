22251362287ff1b0be1e628c387f8ce5570a6ef3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 18:51:06 -0500

    Update up.sql

--- store/migrations/2026-01-09-223955-0000_create_user_update_website/up.sql
+++ store/migrations/2026-01-09-223955-0000_create_user_update_website/up.sql
@@ -6,7 +6,7 @@ ALTER TABLE "website_tick" ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL
 DEFAULT CURRENT_TIMESTAMP;
 
 -- Create user table
-CREATE TABLE "User" {
+CREATE TABLE "user" {
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL
@@ -17,4 +17,4 @@ CREATE TABLE "User" {
 -- Add foreign key relationship between the user that is created now
 -- and the website that already exists
 ALTER TABLE "website" ADD CONSTRAINT "website_user_id_fkey" FOREIGN KEY
-("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
