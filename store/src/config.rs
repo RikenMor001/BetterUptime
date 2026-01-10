72bf790da68cb6ca77f43456d1385d363e64cc4b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 19:57:42 -0500

    Update config.rs

--- store/src/config.rs
+++ store/src/config.rs
@@ -1,16 +1,16 @@
 use std::env;
-use std::error::Error;
 
 pub struct Config {
     pub db_url: String
 }
 
 impl Config {
-    fn default() -> Result<Self, Error> {
+    pub fn default() -> Self {
         let db_url = env::var("DATABASE_URL")
         .unwrap_or_else(|_| panic!("Please provide the database url enviornment variable"));
-        Ok(Self {
+        Self { 
             db_url
-        })
+        }
     }
 }
+
