daa2131223b308e1b88141b39c44d5c4dd9d84aa
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-10 18:00:36 -0500

    Update website.rs

--- store/src/models/website.rs
+++ store/src/models/website.rs
@@ -1,20 +1,20 @@
 use chrono::{NaiveDateTime, Utc};
 use diesel::prelude::*;
 use uuid::Uuid;
-use crate::store::Store;
+use crate::{models::website, store::Store};
 #[derive(Queryable, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::website)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 
 pub struct Website {
-    id: String, 
-    url: String,
-    user_id: Option<String>,
-    time_added: chrono::NaiveDateTime
+    pub id: String, 
+    pub url: String,
+    pub user_id: Option<String>,
+    pub time_added: chrono::NaiveDateTime
 }
 
 impl Store { 
-    pub fn create_website(&mut self, user_id: Option<String>, url: String) {
+    pub fn create_website(&mut self, user_id: Option<String>, url: String) -> Result<Website, diesel::result::Error>{
         let id = Uuid::new_v4().to_string();
         let website = Website{
             user_id,
@@ -22,11 +22,14 @@ impl Store {
             id: id,
             time_added: Utc::now().naive_utc()
         };
-        diesel::insert_into(crate::schema::website::table)
+        let website_data = diesel::insert_into(crate::schema::website::table)
         .values(&website).returning(Website::as_returning())
-        .get_result(&mut self.conn);
+        .get_result(&mut self.conn)?; // Add an question mark just to make sure that the data is unwraped before 
+    // its parsed.
+        Ok(website_data)
     }
 
     pub fn get_website(&self) {
+         
     }
 }
