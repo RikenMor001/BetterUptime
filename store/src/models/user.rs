044cc49a30f5f131cb8f62a2bd51eef52bb57f23
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 20:52:57 -0500

    Update user.rs

--- store/src/models/user.rs
+++ store/src/models/user.rs
@@ -5,25 +5,27 @@ use crate::store::Store;
 #[diesel(table_name = crate::schema::user)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 
-struct User {
+pub struct User {
     id: String, 
     username: String,
     password: String
 }
 
 impl Store {
-    pub fn sign_up(&mut self, username: String, password: String) {
+    pub fn sign_up(&mut self, username: String, password: String) -> Result<String, diesel::result::Error> {
         let id = Uuid::new_v4().to_string(); //Uuid trait not from diesel
         let u = User{
             username,
             password,
-            id: id
+            id: id.clone()
         };
         diesel::insert_into(crate::schema::user::table).values(&u)
         .returning(User::as_returning())
         .get_result(&mut self.conn); // had to import Insertable
+        Ok(id.to_string())
     }
 
     pub fn sign_in(&self, username: String, password: String) {
+
     }
 }
