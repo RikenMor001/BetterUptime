43519aef94dafede636b8d139843663949bceda9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 19:57:18 -0500

    Update store.rs

--- store/src/store.rs
+++ store/src/store.rs
@@ -1,20 +1,19 @@
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::Connection;
-use config::Config;
-use std::error::Error;
+use crate::config::Config;
 
 pub struct Store {
     pub conn: PgConnection
 }
 
 impl Store {
-    pub fn default() -> Result<Self, Error> {
-        let config = Config::default()?;
+    pub fn default() -> Self {
+        let config = Config::default();
 
         let conn = PgConnection::establish(&config.db_url).unwrap_or_else(|_| panic!("Error connecting to the database"));
-        Ok(Self {
+        Self{
             conn
-        })
+        }
     } 
 }
