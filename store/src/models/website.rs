c589f806bf396f7cf172b7410eb429f7ecbd22ba
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-10 17:54:58 -0500

    Update website.rs

--- store/src/models/website.rs
+++ store/src/models/website.rs
@@ -1,4 +1,6 @@
+use chrono::{NaiveDateTime, Utc};
 use diesel::prelude::*;
+use uuid::Uuid;
 use crate::store::Store;
 #[derive(Queryable, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::website)]
@@ -11,8 +13,18 @@ pub struct Website {
     time_added: chrono::NaiveDateTime
 }
 
-impl Store {
-    pub fn create_website(&self) {
+impl Store { 
+    pub fn create_website(&mut self, user_id: Option<String>, url: String) {
+        let id = Uuid::new_v4().to_string();
+        let website = Website{
+            user_id,
+            url,
+            id: id,
+            time_added: Utc::now().naive_utc()
+        };
+        diesel::insert_into(crate::schema::website::table)
+        .values(&website).returning(Website::as_returning())
+        .get_result(&mut self.conn);
     }
 
     pub fn get_website(&self) {
