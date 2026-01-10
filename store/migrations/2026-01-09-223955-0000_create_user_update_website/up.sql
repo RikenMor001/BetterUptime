fb5eb4bbdd60ddd5220b9841ac39de038e46a9ff
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-09 19:55:39 -0500

    Update up.sql

--- store/migrations/2026-01-09-223955-0000_create_user_update_website/up.sql
+++ store/migrations/2026-01-09-223955-0000_create_user_update_website/up.sql
@@ -1,20 +1,22 @@
 -- Your SQL goes here
-ALTER TABLE "website" ADD COLUMN "user_id" TEXT NOT NULL;
 
---Alter table
-ALTER TABLE "website_tick" ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL
-DEFAULT CURRENT_TIMESTAMP;
+-- Add user_id to website (safe even if website already has rows)
+ALTER TABLE "website" ADD COLUMN "user_id" TEXT;
+
+-- Add updated_at to website_tick
+ALTER TABLE "website_tick"
+ADD COLUMN "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 -- Create user table
-CREATE TABLE "user" {
+CREATE TABLE "user" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL
+    "password" TEXT NOT NULL,
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
 
-    CONSTRAINT "user_pkey" PRIMARY KEY("id")
-};
-
--- Add foreign key relationship between the user that is created now
--- and the website that already exists
-ALTER TABLE "website" ADD CONSTRAINT "website_user_id_fkey" FOREIGN KEY
-("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- Add foreign key relationship
+ALTER TABLE "website"
+ADD CONSTRAINT "website_user_id_fkey"
+FOREIGN KEY ("user_id") REFERENCES "user"("id")
+ON DELETE RESTRICT ON UPDATE CASCADE;
