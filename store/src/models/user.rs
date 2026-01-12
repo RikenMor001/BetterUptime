5a323676c2b9e5e5e4001c594d0418ab7fa537c4
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-12 10:09:54 -0500

    Update user.rs

--- store/src/models/user.rs
+++ store/src/models/user.rs
@@ -1,6 +1,8 @@
 use diesel::{prelude::*};
 use uuid::Uuid;
 use crate::store::Store;
+use argonautica::Hasher;
+use dotenv::dotenv;
 #[derive(Queryable, Selectable, Insertable)]
 #[diesel(table_name = crate::schema::user)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
@@ -14,10 +16,13 @@ pub struct User {
 
 impl Store {
     pub fn sign_up(&mut self, username: String, password: String) -> Result<String, diesel::result::Error> {
+        dotenv().ok();
+        let secret_message = std::env::var("SECRET_KEY").expect("SECRET_KEY expected here, not found");
+        let hashed_password = Hasher::default().with_password(password).with_secret_key(secret_message).hash().unwrap();
         let id = Uuid::new_v4().to_string(); //Uuid trait not from diesel
         let u = User{
             username,
-            password,
+            password: hashed_password,
             id: id.clone()
         };
         diesel::insert_into(crate::schema::user::table).values(&u)
