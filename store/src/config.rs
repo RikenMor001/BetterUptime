86c492770d780792173e3e3a55fe7b99012d33fc
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-12 09:59:20 -0500

    Update config.rs

--- store/src/config.rs
+++ store/src/config.rs
@@ -5,12 +5,12 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn default() -> Result<Self, diesel::result::Error> {
+    pub fn default() -> Self {
         let db_url = env::var("DATABASE_URL")
         .unwrap_or_else(|_| panic!("Please provide the database url enviornment variable"));
-        Ok(Self{
+        Self{
             db_url
-        })
+        }
     }
 }
 
