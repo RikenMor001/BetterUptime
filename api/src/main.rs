fbfd73262fe96d522240fe745a0085f0ba5c2404
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-13 18:46:45 -0500

    Update main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -12,16 +12,30 @@ pub mod outputs;
 
 use inputs::CreateWebsiteInput;
 use outputs::CreateWebsiteOutput;
+
 use store::store::Store;
+use crate::{ inputs::CreateUserInput, outputs::CreateUserOutput };
 
 #[handler]
 fn get_website(Path(website_id): Path<String>) -> String {
-    format!("BetterUptime: {website_id}")
+    format!("BetterUptime: {website_id}" )
+}
+
+#[handler]
+fn user_sign_in(Json(data): Json<CreateUserInput>) -> Result<Json<CreateUserOutput>, Error> {
+    let mut s = Store::default()
+        .map_err(|e| Error::from_string(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+
+    let id = s
+        .sign_up(data.username, data.password)
+        .map_err(|e| Error::from_string(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+
+    Ok(Json(CreateUserOutput { id }))
 }
 
 #[handler]
 async fn create_website(
-    Json(data): Json<CreateWebsiteInput>,
+    Json(data): Json<CreateWebsiteInput>
 ) -> Result<Json<CreateWebsiteOutput>, Error> {
     let mut s: Store = Store::default().unwrap();
 
