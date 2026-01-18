b157756e5b7f1e2cd3143d74f7526700322e3777
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-17 19:03:12 -0500

    Update validation.rs

--- api/src/auth/validation.rs
+++ api/src/auth/validation.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use Uuid::uuid;
-use jsonwebtoken::{Encode, Decode};
+use jsonwebtoken::{encode, decode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use chrono::{Duration, Utc};
 
 pub struct Jwt{
@@ -17,4 +17,22 @@ pub fn sign_jwt(user_id: &str, secret: &str) -> Result <String, jsonwebtoken::er
         jwt_created_at: current_timestamp.timestamp() as usize,
         jwt_expired_at: (current_timestamp + Duration::hours(24)).timestamp() as usize
     };
+
+    encode(
+        &Header::new(Algorithm::HS256),
+        &jwt_impl,
+        &EncoidngKey::from_secret(secret.as_bytes())
+    )
+}
+
+pub fn verify_jwt(token: &str, secret: &str) -> Result<Jwt, jsonwebtoken::errors::Error> {
+    let validation = Validation::new(Algorithm::HS256);
+
+    let data = decode::<Jwt>(
+        token,
+        DecodingKey::from_secret(secret.as_bytes()),
+        &validation
+    )?;
+
+    Ok(data.jwt_impl)
 }
