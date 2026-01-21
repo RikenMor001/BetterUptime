e1d53e36a4e7718fc6c7be9c7f889ae7e780e6d3
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-21 18:10:02 -0500

    Update health.rs

--- api/src/auth/health.rs
+++ api/src/auth/health.rs
@@ -1,10 +1,10 @@
 // Check whether if the user exists
 use diesel::prelude::*;
 use store::schema::user;
-use crate::auth::extractor::AuthUser;
+use crate::Store::default::conn;
 
 pub fn check_user_existense(user_id: String) -> Result<(bool, String), diesel::result::Error>{
-    let mut checkConn = establish_connection()?;
+    let mut checkConn = conn()?;
 
     // Will get the user from the database
     // Try to match the user if they match correctly
