569afbbe22ad211ea1ce0a88288dcc7fd1fe347d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-21 18:05:35 -0500

    Update health.rs

--- api/src/auth/health.rs
+++ api/src/auth/health.rs
@@ -1,14 +1,16 @@
 // Check whether if the user exists
-
+use diesel::prelude::*;
+use store::schema::user;
 use crate::auth::extractor::AuthUser;
 
-pub struct UserExists{
-    user_id: String
-}
+pub fn check_user_existense(user_id: String) -> Result<(bool, String), diesel::result::Error>{
+    let mut checkConn = establish_connection()?;
 
-pub fn check_user_existense(user_id: String) -> Result<String, diesel::result::Error>{
-    let user_check = AuthUser{
-        user_id: user_id
+    // Will get the user from the database
+    // Try to match the user if they match correctly
+
+    match user{
+        Some(_) => Ok(true ,"User exists in the database".to_string()),
+        None(_) => Ok(false, "User does not exist or the server is down".to_string());
     };
-    
 }
