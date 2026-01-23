956470c8e06beaa9543381690227bcf80abe9bcf
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-22 23:53:01 -0500

    Update health.rs

--- api/src/auth/health.rs
+++ api/src/auth/health.rs
@@ -6,4 +6,16 @@ use store::schema::user::dsl::*;
 pub fn check_user_health(user_id: String) -> Result<(bool, String), diesel::result::Error> {
     let mut store = Store::default()?;
     let conn = &mut store.conn;
+
+    // users.fiilter.select.first.optional
+    let user = users
+    .filter(id.eq(&user_id))
+    .select(id)
+    .first::<String>(conn)
+    .optional()?;
+
+    match user{
+        Some(_) => Ok((true, "User exists and server is up".to_string())),
+        None(_) => Ok((false, "Either user doesn't exist or the server is down".to_string())),
+    }
 }
