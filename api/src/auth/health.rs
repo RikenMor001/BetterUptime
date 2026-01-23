ee338e1cf0a93597d9fa00c03fe33fb3b74a1cc8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-23 00:15:55 -0500

    Update health.rs\

--- api/src/auth/health.rs
+++ api/src/auth/health.rs
@@ -2,8 +2,26 @@ use diesel::prelude::*;
 use store::store::Store;
 use store::schema::user::dsl::*;
 
+// Making a custom enum 
+pub enum HealthError{
+    Connection(diesel::ConnectionError),
+    Query(diesel::result::Error)
+}
+
+impl From<diesel::ConnectionError> for HealthError{
+    fn from(value: diesel::ConnectionError) -> Self {
+        Self::Connection((value))
+    }
+}
+
+impl From<diesel::result::Error> for HealthError{
+    fn from(value: diesel::result::Error) -> Self {
+        Self::Query((value))
+    }
+}       
+
 // Check connection 
-pub fn check_user_health(user_id: String) -> Result<(bool, String), diesel::ConnectionError> {
+pub fn check_user_health(user_id: String) -> Result<(bool, String), HealthError> {
     let mut store = Store::default()?;
     let conn = &mut store.conn;
 
@@ -12,10 +30,10 @@ pub fn check_user_health(user_id: String) -> Result<(bool, String), diesel::Conn
     .filter(id.eq(&user_id))
     .select(id)
     .first::<String>(conn)
-    .optional();
+    .optional()?;
 
     match found{
         Some(_) => Ok((true, "User exists and server is up".to_string())),
-        None(_) => Ok((false, "Either user doesn't exist or the server is down".to_string())),
+        None => Ok((false, "User does not exist".to_string()))
     }
 }
