ad734b5ac4211eabf24c8233c7ba867d4df2596a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-26 17:34:56 -0500

    Update user.rs

--- store/src/models/user.rs
+++ store/src/models/user.rs
@@ -23,17 +23,23 @@ pub enum AuthError{
 impl Store {
     pub fn sign_up(&mut self, username: String, password: String) -> Result<String, diesel::result::Error> {
         dotenv().ok();
-        let secret_message = std::env::var("SECRET_KEY").expect("SECRET_KEY expected here, not found");
-        let hashed_password = Hasher::default().with_password(password).with_secret_key(secret_message).hash().unwrap();
+        let secret_message = std::env::var("SECRET_KEY")
+        .map_err(|_| diesel::result::Error::RollbackTransaction)?;
+
+        let hashed_password = Hasher::default().with_password(password).with_secret_key(secret_message).hash()
+        .map_err(|_| diesel::result::Error::RollbackTransaction)?;
+
         let id = Uuid::new_v4().to_string(); //Uuid trait not from diesel
+
         let u = User{
+            id: id.clone(),
             username,
-            password: hashed_password,
-            id: id.clone()
+            password: hashed_password
         };
+        
         diesel::insert_into(crate::schema::user::table).values(&u)
-        .returning(User::as_returning())
-        .get_result(&mut self.conn)?; // had to import Insertable
+        .execute(&mut self.conn)?;// had to import Insertable
+
         Ok(id.to_string())
     }
 
@@ -41,7 +47,7 @@ impl Store {
         use crate::schema::user::dsl::*;
 
         let user_data = user // fetches the user data and returns it
-        .filter(id.eq(ref_username))
+        .filter(username.eq(ref_username))
         .select(User::as_select())
         .first(&mut self.conn)
         .map_err(|e| match e {
