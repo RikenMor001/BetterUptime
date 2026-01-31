68f77ae8b8c6dd2bc10c5607355602cbbc7f5c49
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-01-31 01:58:38 -0500

    Update store.rs

--- store/src/store.rs
+++ store/src/store.rs
@@ -1,6 +1,10 @@
 use diesel::pg::PgConnection;
-use diesel::Connection;
+use diesel::{Connection, RunQueryDsl};
+use diesel::query_dsl::methods::SelectDsl;
 use crate::config::Config;
+use diesel::SelectableHelper;
+
+use crate::models::website::Website;
 
 pub struct Store {
     pub conn: PgConnection
@@ -10,9 +14,16 @@ impl Store {
     pub fn default() -> Result<Self, diesel::ConnectionError>{
         let config = Config::default();
 
-        let conn = PgConnection::establish(&config.db_url)?;
+        let conn: PgConnection = PgConnection::establish(&config.db_url)?;
         Ok(Self { 
             conn 
-        }) // Write diesel::ConnectionError instead of unwrapping, makes it easy
+        }) // Had to return Result<Self, diesel::result::Error> for using .unwrap()
     } 
+
+    pub fn list_websites(&mut self) -> Result<Vec<Website>, diesel::result::Error>{
+        use crate::schema::website::dsl::*;
+
+        let rows = website.select(Website::as_select()).load(&mut self.conn)?;
+        Ok(rows)
+    }
 }
