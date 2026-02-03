5d1eb6653e9261a66eab5444b2ad922962d09cde
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-02 23:27:12 -0500

    Update outputs.rs

--- api/src/outputs.rs
+++ api/src/outputs.rs
@@ -3,6 +3,7 @@ use serde::{ Deserialize, Serialize};
 #[derive(Deserialize, Serialize)]
 pub struct CreateWebsiteOutput {
     pub id: String,
+    pub msg: String
 }
 
 #[derive(Deserialize, Serialize)]
