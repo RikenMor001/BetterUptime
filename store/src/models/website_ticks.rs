23ee0dd633ab0d8c725c8688637ebedc5f0fd2d9
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-07 21:30:10 -0500

    Update website_ticks.rs

--- store/src/models/website_ticks.rs
+++ store/src/models/website_ticks.rs
@@ -9,6 +9,7 @@ use crate::schema::website_tick::dsl::*;
 #[diesel(table_name = website_tick)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 
+// Structuring the struct according to the website_tick table in schema
 pub struct WebsiteTick {
     pub id: String,
     pub website_id: String,
@@ -19,7 +20,16 @@ pub struct WebsiteTick {
 }
 
 impl Store {
-    pub fn save_website_check_results(){
+    pub fn save_website_check_results() -> Result<WebsiteTick, diesel::result::Error>{
         let id = Uuid::new_v4().to_string();
+
+        let tick = WebsiteTick{
+            id: id, 
+            website_id,
+            region_id,
+            response_time_ms,
+            status,
+            updated_at: Utc::now().naive_utc()
+        };
     }
 }
