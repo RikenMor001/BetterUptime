22ba2a55c3fedd660be8bcfe948fa77917fc086d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-07 21:21:11 -0500

    Update website_ticks.rs

--- store/src/models/website_ticks.rs
+++ store/src/models/website_ticks.rs
@@ -1,17 +1,21 @@
 use crate::store::Store;
 use diesel::prelude::*;
 use uuid::Uuid;
+use chrono::NaiveDateTime;
+use crate::schema::sql_types::WebsiteStatus;
+use crate::schema::website_tick::dsl::*;
 
 #[derive(Queryable, Selectable, Insertable)]
-#[diesel(table_name = website_ticks)]
+#[diesel(table_name = website_tick)]
 #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 
 pub struct WebsiteTick {
-    pub id: String, 
+    pub id: String,
     pub website_id: String,
+    pub region_id: String,
     pub response_time_ms: i32,
     pub status: WebsiteStatus,
-    
+    pub updated_at: NaiveDateTime
 }
 
 impl Store {
