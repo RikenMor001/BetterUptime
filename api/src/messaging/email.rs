b94bf76ae27da7169d0b485d872b8431d98eacec
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-18 18:14:53 -0500

    Update email.rs

--- api/src/messaging/email.rs
+++ api/src/messaging/email.rs
@@ -5,4 +5,9 @@ pub fn send_downtime_alert(to_email: &str, website_url: &str, status: &str) -> R
     let smtp_user = std::env::var("SMTP_USER").map_err(|_| "SMTP user not found")?;
     let smtp_password = std::env::var("SMTP_PASSWORD").map_err(|_| "SMTP password not found")?;
 
+    let creds = Credentials::new(smtp_user, smtp_password);
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+    .map_err(|_| "Failed to connect to the SMTP server")?
+    .credentials(creds)
+    .build();
 }
