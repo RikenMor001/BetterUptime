3000540cff10c521d3566cd8401678bf8394c5f4
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-18 00:06:15 -0500

    Update schema.rs

--- store/src/schema.rs
+++ store/src/schema.rs
@@ -1,3 +1,5 @@
+// @generated automatically by Diesel CLI.
+
 pub mod sql_types {
     #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
     #[diesel(postgres_type(name = "website_status"))]
@@ -5,19 +7,17 @@ pub mod sql_types {
 }
 
 diesel::table! {
-    use diesel::sql_types::*;
-    use crate::schema::sql_types::WebsiteStatus;
-
-    website (id) {
+    notification (id) {
         id -> Text,
-        url -> Text,
-        time_added -> Timestamptz,
+        user_id -> Text,
+        email -> Text,
+        notify_down -> Bool,
+        notify_up -> Bool,
+        created_at -> Timestamptz,
     }
 }
 
 diesel::table! {
-    use diesel::sql_types::*;
-
     region (id) {
         id -> Text,
         name -> Text,
@@ -25,8 +25,25 @@ diesel::table! {
 }
 
 diesel::table! {
+    user (id) {
+        id -> Text,
+        username -> Text,
+        password -> Text,
+    }
+}
+
+diesel::table! {
+    website (id) {
+        id -> Text,
+        url -> Text,
+        time_added -> Timestamptz,
+        user_id -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     use diesel::sql_types::*;
-    use crate::schema::sql_types::WebsiteStatus;
+    use super::sql_types::WebsiteStatus;
 
     website_tick (id) {
         id -> Text,
@@ -34,14 +51,13 @@ diesel::table! {
         region_id -> Text,
         response_time_ms -> Int4,
         status -> WebsiteStatus,
+        updated_at -> Timestamp,
     }
 }
 
+diesel::joinable!(notification -> user (user_id));
+diesel::joinable!(website -> user (user_id));
 diesel::joinable!(website_tick -> region (region_id));
 diesel::joinable!(website_tick -> website (website_id));
 
-diesel::allow_tables_to_appear_in_same_query!(
-    website,
-    region,
-    website_tick,
-);
+diesel::allow_tables_to_appear_in_same_query!(notification, region, user, website, website_tick,);
