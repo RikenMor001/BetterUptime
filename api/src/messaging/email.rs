1d3c92527fbd1e9422dbbd2d4e85da5c7c68a47a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-18 18:12:00 -0500

    Update email.rs

--- api/src/messaging/email.rs
+++ api/src/messaging/email.rs
@@ -1,4 +1,8 @@
 use lettre::{Message, SmtpTransport, Transport};
+use letter::transport::smtp::authentication::Credentials;
+
 pub fn send_downtime_alert(to_email: &str, website_url: &str, status: &str) -> Result<(), String>{
+    let smtp_user = std::env::var("SMTP_USER").map_err(|_| "SMTP user not found")?;
+    let smtp_password = std::env::var("SMTP_PASSWORD").map_err(|_| "SMTP password not found")?;
 
 }
