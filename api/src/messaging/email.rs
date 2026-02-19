c2ce3e6fb860e2128ece977f242c832499223c7a
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-19 18:01:35 -0500

    Update email.rs

--- api/src/messaging/email.rs
+++ api/src/messaging/email.rs
@@ -6,17 +6,17 @@ pub fn send_downtime_alert(to_email: &str, website_url: &str, status: &str) -> R
     let smtp_password = std::env::var("SMTP_PASSWORD").map_err(|_| "SMTP password not found")?;
 
     let email = Message::builder()
-    .from("BetterUptime <no-reply@gmail.com>".parse().unwrap())
-    .to(to_email.parse().map_err(|e| e.to_string())?)
-    .subject("Downtime Alert")
-    .body(format!("The website {} is now {}", website_url, status))
-    .map_err(|e| e.to_string())?;
+        .from("BetterUptime <no-reply@gmail.com>".parse().unwrap())
+        .to(to_email.parse().map_err(|e| e.to_string())?)
+        .subject("Downtime Alert")
+        .body(format!("The website {} is now {}", website_url, status))
+        .map_err(|e| e.to_string())?;
 
     let creds = Credentials::new(smtp_user, smtp_password);
     let mailer = SmtpTransport::relay("smtp.gmail.com")
-    .map_err(|_| "Failed to connect to the SMTP server")?
-    .credentials(creds)
-    .build();
+        .map_err(|e| e.to_string())?
+        .credentials(creds)
+        .build();
 
     mailer.send(&email).unwrap_or_else(|_| panic!("Failed to send email"))?;
     Ok(())
