9406ba298c7c92b2ee626f26754223133e6d94cf
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-21 22:25:14 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div className='text-red text-center'>
+    Frontend incoming
+  </div>
 }
 
 export default App
