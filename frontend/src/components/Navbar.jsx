b45f19387984b265acd799a843fbffc57cffc7d7
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-26 19:10:21 -0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,8 @@ export default function NavBar(){
         BetterUptime
       </div>
       <div>
-        <button className="border border-slate-300 shadow-sm rounded-md px-4 py-2 text-slate-600 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center">
+        <button className="border border-slate-500 shadow-sm rounded-2xl px-4 py-2 text-slate-600 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
+        hover:text-slate-900 hover:border-slate-900 hover:bg-slate-900 hover:text-white font-semibold transition-transform duration-300 hover:-translate-y-0.5">
           Sign up
         </button>
       </div>
