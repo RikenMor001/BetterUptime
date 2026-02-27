232d00a23e4742d436840b03f8b9e343aeb7c8ed
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-02-26 19:11:13 -0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,10 +5,14 @@ export default function NavBar(){
     hover:text-slate-900 hover:cursor-pointer transition-transform duration-300 hover:-translate-y-0.5">
         BetterUptime
       </div>
-      <div>
+      <div className="flex gap-4">
         <button className="border border-slate-500 shadow-sm rounded-2xl px-4 py-2 text-slate-600 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
         hover:text-slate-900 hover:border-slate-900 hover:bg-slate-900 hover:text-white font-semibold transition-transform duration-300 hover:-translate-y-0.5">
           Sign up
+        </button>
+        <button className="border border-slate-500 shadow-sm rounded-2xl px-4 py-2 text-slate-600 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
+        hover:text-slate-900 hover:border-slate-900 hover:bg-slate-900 hover:text-white font-semibold transition-transform duration-300 hover:-translate-y-0.5">
+          Sign in
         </button>
       </div>
     </div>
