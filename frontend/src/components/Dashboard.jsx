6b93105b27916fd02f6d39f6951a04257f2ca29f
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-03-02 17:07:07 -0500

    Update Dashboard.jsx

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,13 @@
 export default function Dashboard(){
-    return <div className="bg-slate-900 h-screen flex items-center justify-center">
-        <div className="text-center text-slate-100 text-4xl font-bold tracking-tght">
-            The most reliable <br/>
-            uptime monitoring service            
+    return <div className="bg-neutral-800 h-screen flex items-center justify-center">
+        <div className="text-center text-stone-50">
+            <div className="text-center text-stone-50 text-5xl font-bold tracking-tght">
+                The most reliable <br/>
+                uptime monitoring service 
+            </div>
+            <div className="text-center text-stone-400 text-xl font-medium mt-4">
+                <p>Get 10 monitors, 10 heartbeats and a status page <br/>with 3-minute checks totally free.</p>    
+            </div> 
         </div>
     </div>
 }
