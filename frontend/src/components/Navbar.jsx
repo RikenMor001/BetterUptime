057f3fc9a704bdb7ff80349295e68c42bed44189
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-03-02 17:06:57 -0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 export default function NavBar(){
   return <div>
-    <div className="bg-slate-900 justify-between items-center flex px-4 py-3 shadow-xl fixed w-full z-50 border-b border-gray-200">
+    <div className="bg-neutral-900 justify-between items-center flex px-4 py-2 shadow-xl fixed w-full z-50 border-b border-neutral-800 h-14">
       <div className="text-slate-100 font-bold text-xl hover:text-slate-200 hover:cursor-pointer transition-transform duration-300 hover:-translate-y-0.5">
         BetterUptime
       </div>
       <div className="flex gap-4">
-        <button className="border border-slate-200 shadow-sm rounded-2xl px-4 py-2 text-slate-300 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
+        <button className="border border-slate-200 shadow-sm rounded-2xl px-4 py-1 text-slate-300 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
         hover:text-slate-900 hover:border-white hover:bg-slate-700 hover:text-white font-semibold transition-transform duration-300 hover:-translate-y-0.5">
           Sign in
         </button>
-        <button className="border border-slate-200 shadow-sm rounded-2xl px-4 py-2 text-slate-300 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
+        <button className="border border-slate-200 shadow-sm rounded-2xl px-4 py-1 text-slate-300 hover:bg-slate-200 hover:cursor-pointer transition-colors duration-300 text-sm font-medium text-center
         hover:text-slate-900 hover:border-white hover:bg-slate-700 hover:text-white font-semibold transition-transform duration-300 hover:-translate-y-0.5">
           Sign up
         </button>
