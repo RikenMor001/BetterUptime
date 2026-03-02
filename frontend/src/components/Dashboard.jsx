8c20ba01d208f792496fe040f23a88c430ebbed8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2026-03-01 21:05:49 -0500

    Add files via upload

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard(){
+    return <div className="bg-slate-900 h-screen flex items-center justify-center">
+        <div className="text-center text-slate-100 text-4xl font-bold tracking-tght">
+            The most reliable <br/>
+            uptime monitoring service            
+        </div>
+    </div>
+}
